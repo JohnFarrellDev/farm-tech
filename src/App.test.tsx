6e2e88d2c1f2b6cf8bcdb645750d6dbe6955e64a
JohnFarrellDev <johnfarrell@fastmail.com>
2021-03-04 14:25:15 +0000

    remove unused import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-import { calculateTotalCost } from './utils/calculateTotalCost';
 
 describe("App.tsx", () => {
   it("should render", () => {
