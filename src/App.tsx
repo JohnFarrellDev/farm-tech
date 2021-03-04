79def49d919704b9b0ca86a8b8906ecdba86271f
Felix Dux <felixdux@Felix-Dux-MBP.broadband>
2021-03-04 10:41:54 +0000

    Showing instructions

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,14 @@ function App() {
   const [numberOfBagsOfCorn, setNumberOfBagsOfCorn] = useState(0)
   const [numberOfGeese, setNumberOfGeese] = useState(0)
   const [numberOfExtraCrossings, setNumberOfExtraCrossings] = useState(0)
+  const [instructions, setInstructions] = useState('');
   const [willCornBeLost, setWillCornBeLost] = useState(false);
 
   useEffect(() => {
-    setWillCornBeLost(willSomeCornBeLost(numberOfBagsOfCorn, numberOfGeese));
+    const result = willSomeCornBeLost(numberOfBagsOfCorn, numberOfGeese);
+    setWillCornBeLost(result.losingCorn);
+    setNumberOfExtraCrossings(result.extraTrips);
+    setInstructions(result.instructions);
   }, [numberOfBagsOfCorn, numberOfGeese])
 
   const updatedNumberOfBagsOfCorn = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -44,7 +48,7 @@ function App() {
         value={numberOfGeese}
       />
       <p data-testid="total-cost">This is the total cost of taking corn and geese to market and coming home again: £{calculateTotalCost(numberOfBagsOfCorn + numberOfGeese + numberOfExtraCrossings)}</p>
-      {willCornBeLost ? <p>"Warning: Geese will eat your corn!"</p>: null}
+      {willCornBeLost ? <p><em>Warning: Geese will eat your corn!</em></p>: instructions === '' ? null : <p>{instructions}</p>}
     </AppContainer>
   );
 }
